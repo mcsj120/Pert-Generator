b323608262c936f77f66ba3875bc1336a5ee787c
Matthew Bohr <matthew1up@gmail.com>
2018-09-28 10:28:05 -0700

    Add a listener to the Starting Node checkbox
    Turns off dependencies text box when it's checked

--- Network analysis explorer/src/NodeEntryUIPanel.java
+++ Network analysis explorer/src/NodeEntryUIPanel.java
@@ -1,6 +1,8 @@
 /*This class creates a JPanel with the main part of the UI
  * Written by Matthew Bohr
  *  */
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.*;
 
 public class NodeEntryUIPanel extends JPanel {
@@ -16,11 +18,31 @@ public class NodeEntryUIPanel extends JPanel {
 		help.setText("Help");
 		restart.setText("Exit");
 		isStartingNode.setText("Starting Node");
+		isStartingNode.setSelected(false);
 		nameLabel.setText("Activity name:");
 		durationLabel.setText("Duration:");
 		dependenciesLabel.setText("Dependencies");
+		isStartingNode.addItemListener(new StartingNodeListener());
+		nodeName.setEditable(true);
+		nodeDuration.setEditable(true);
+		nodeDependencies.setEditable(true);
 		
 		
 	};
+	//Listener for Starting node check box that turns off the dependencies text field if the check box is checked
+	public class StartingNodeListener implements ItemListener{
+
+		@Override
+		public void itemStateChanged(ItemEvent event) {
+			if (event.getStateChange()==ItemEvent.SELECTED) {
+				nodeDependencies.setEditable(false);
+			} else {
+				nodeDependencies.setEditable(true);
+			}
+
+		}
+		
+	}
+	
 
 }
