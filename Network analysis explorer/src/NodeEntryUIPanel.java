2470e6ae53c48e2d2c5b2014fffea14967c98ff4
Matthew Bohr <matthew1up@gmail.com>
2018-10-02 18:34:35 -0700

    Update the panel with another actionlistener

--- Network analysis explorer/src/NodeEntryUIPanel.java
+++ Network analysis explorer/src/NodeEntryUIPanel.java
@@ -1,31 +1,38 @@
 /*This class creates a JPanel with the main part of the UI
  * Written by Matthew Bohr
  *  */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import javax.swing.*;
 
 public class NodeEntryUIPanel extends JPanel {
+	
 	private JButton addNode,analyze,about,help,restart,exit;
 	private JTextField nodeName,nodeDuration,nodeDependencies;
 	private JCheckBox isStartingNode;
 	private JLabel nameLabel,durationLabel,dependenciesLabel;
 	
+	//Constructor
 	public NodeEntryUIPanel() {
 		addNode.setText("Add Node");
 		analyze.setText("Analyze Network");
 		about.setText("About");
 		help.setText("Help");
-		restart.setText("Exit");
+		restart.setText("Restart");
+		exit.setText("Exit");
 		isStartingNode.setText("Starting Node");
 		isStartingNode.setSelected(false);
 		nameLabel.setText("Activity name:");
 		durationLabel.setText("Duration:");
 		dependenciesLabel.setText("Dependencies");
-		isStartingNode.addItemListener(new StartingNodeListener());
 		nodeName.setEditable(true);
 		nodeDuration.setEditable(true);
 		nodeDependencies.setEditable(true);
+		isStartingNode.addItemListener(new StartingNodeListener());
+		addNode.addActionListener(new AddNodeListener());
+		
 		
 		
 	};
@@ -43,6 +50,14 @@ public class NodeEntryUIPanel extends JPanel {
 		}
 		
 	}
-	
+	public class AddNodeListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent action) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
 
 }
