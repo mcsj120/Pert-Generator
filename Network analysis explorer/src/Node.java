7317639311f9b6a1789feb89bf7cf1039023291a
Michael St Onge <mcstonge@asu.edu>
2018-10-02 19:12:19 -0700

    Expanded Node class
    Added getters, setters, and ArrayList to node class

--- Network analysis explorer/src/Node.java
+++ Network analysis explorer/src/Node.java
@@ -1,10 +1,58 @@
+import java.util.ArrayList;
+
 /*This class represents a node
  * Written by Jacob Baca
+ * Last edited by Michael St Onge
  *  */
+
 public class Node {
 	private String activityName;
 	private int duration;
-	private Node[] dependencies;
+	private ArrayList<Node> dependencies;
+	private ArrayList<Node> ancestors;
+	private boolean head;
 	
+	public Node(String activityName, int duration)
+	{
+		this.activityName = activityName;
+		this.duration = duration;
+		this.head = true;
+	}
 	
+	public Node(String activityName, int duration, ArrayList<Node> dependencies)
+	{
+		this.activityName = activityName;
+		this.duration = duration;
+		this.dependencies = dependencies;
+		this.head = false;
+	}
+	
+	public String getName()
+	{
+		return this.activityName;
+	}
+	
+	public int getDuration()
+	{
+		return this.duration;
+	}
+	
+	public ArrayList<Node> getDependencies()
+	{
+		return this.dependencies;
+	}
+	
+	public ArrayList<Node> getAncestors()
+	{
+		return this.ancestors;
+	}
+	
+	public void setDependencies(ArrayList<Node> dependencies)
+	{
+		this.dependencies = dependencies;
+	}
+	public void setAncestors(ArrayList<Node> ancestors)
+	{
+		this.ancestors = ancestors;
+	}
 }
