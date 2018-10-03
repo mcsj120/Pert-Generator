8e321d9cc7aab299948ee372652cb8cc5250bbd0
Michael St Onge <mcstonge@asu.edu>
2018-10-03 09:26:26 -0700

    Removed duration elements
    Went back through design documentation and realized that we need invidual durations too, so we'll need to extract those individually from each element in the list

--- Network analysis explorer/src/PathData.java
+++ Network analysis explorer/src/PathData.java
@@ -8,37 +8,9 @@ public class PathData
 	public ArrayList<Node> path;
 	public int duration;
 	
-	/*
-	 * Remove Exception in final build. Exception is for error checking
-	 */
-	public PathData(ArrayList<Node> path, int duration) throws Exception
+	public PathData(ArrayList<Node> path, int duration)
 	{
 		this.path = path;
-		this.duration = duration;
-		boolean test = this.errorCheck();
-		if(!test)
-		{
-			throw new Exception("BadInputs");
-		}
 	}
 	
-	/*
-	 * This function tests to make sure that the duration placed in pathData matches the actual duration
-	 */
-	private boolean errorCheck()
-	{
-		int tempDuration = 0;
-		for(int i = 0; i < path.size();i++)
-		{
-			tempDuration += path.get(i).getDuration();
-		}
-		if(tempDuration == duration)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
 }
