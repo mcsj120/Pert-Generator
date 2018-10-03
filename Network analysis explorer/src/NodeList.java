ddb0d264b8f93ae0d42cd5b3736e4f7b957bee55
Michael St Onge <mcstonge@asu.edu>
2018-10-02 23:23:33 -0700

    Stores Nodes before analysis
    This is a singleton class that stores the nodes before sorting them.

---
+++ Network analysis explorer/src/NodeList.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class NodeList 
+{
+		private static NodeList instance = null;
+		
+	   	private ArrayList<Node> list = new ArrayList<Node>();
+	   	
+	   	
+		private NodeList() 
+	   	{ 
+	         
+	   	} 
+	  
+	    // static method to create instance of NodeList class 
+	    public static NodeList getInstance() 
+	    { 
+	        if (instance == null) 
+	        {
+	            instance = new NodeList(); 
+	        }
+ 	        return instance; 
+	    }
+	    
+	    /*
+	     *	Sets the list values 
+	     */
+	    public void setNodeList(ArrayList<Node> list)
+	    {
+	    	this.list = list;
+	    }
+	    
+	    /*
+	     * Returns the list
+	     */
+	    public ArrayList<Node> getNodeList()
+	    {
+	    	return this.list;
+	    }
+	    
+	    /*
+	     * Resets the values in the List
+	     */
+	    public void resetList()
+	    {
+	    	this.list = null;
+	    }
+	    
+	    /*
+	     * Adds a single node to the list
+	     */
+	    public void addToList(Node task)
+	    {
+	    	list.add(task);
+	    }
+}
