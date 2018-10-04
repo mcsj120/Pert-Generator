d9aede646f17f2f3ef1685df17ba6c4eafed41b5
Michael St Onge <mcstonge@asu.edu>
2018-10-03 21:42:16 -0700

    Improved Organizer Class
    Added Getters and Setters
    Also added some function templates so that you guys can wrrite some of your own functions

--- Network analysis explorer/src/Organizer.java
+++ Network analysis explorer/src/Organizer.java
@@ -6,43 +6,97 @@ import java.util.ArrayList;
 //the class that holds all of the logic 
 public class Organizer
 {
-    public ArrayList<PathData> pathList; 
-    public NodeList list;
+    private ArrayList<PathData> pathList; 
+    private NodeList list;
+    private boolean valid;
+    
+    public Organizer(NodeList list)
+    {
+        this.setList(list);
+    }
 
-    public static void errorDependencyCheck()
+    public void errorDependencyCheck()
     {
         //checking to make sure the strings they said are depency match to what they said
     }
-
-    public static boolean errorDuplicateCheck(NodeList list)
+    
+    public void errorCircular()
     {
-        boolean exists = false;
-        ArrayList<String> names = new ArrayList<String>();
-        for(int i = 0; i < list.list.size(); i ++)
+    	//checks to make sure no circular paths exist
+    }
+
+    public void errorDuplicateCheck(NodeList list)
+    {
+    	/*
+    	 * If the function is already valid, we don't need to test it
+    	 */
+        if(valid == true)
         {
-            for(int j = 0; j< names.size(); j++ )
-            {
-                if(list.list.get(i).getName().equals(names.get(i)))
-                {
-                    exists = true;
-                    break;
-                }
-            }
-            if(!exists)
-            {
-                names.add(list.list.get(i).getName());
-            }
-            else
-            {
-                break;
-            }
+        	boolean exists = false;
+	        ArrayList<String> names = new ArrayList<String>();
+	        for(int i = 0; i < list.list.size(); i ++)
+	        {
+	            for(int j = 0; j< names.size(); j++ )
+	            {
+	                if(list.list.get(i).getName().equals(names.get(i)))
+	                {
+	                    exists = true;
+	                    break;
+	                }
+	            }
+	            if(!exists)
+	            {
+	                names.add(list.list.get(i).getName());
+	            }
+	            else
+	            {
+	                break;
+	            }
+	        }
         }
-        return exists;
+    }
+    
+    public void findDependencies()
+    {
+    	//This function adds all descendants to a Node, including the dependencies of their dependencies
+    }
+    
+    public void findAncestors()
+    {
+    	//This function adds all ancestors to a Node, including the ancestors of their ancestors
     }
 
-    public Organizer(NodeList list)
-    {
-        this.list = list;
-    }
+    /*
+     * Getters and Setters
+     */
+	public boolean isValid() 
+	{
+		return valid;
+	}
+
+	public void setValid(boolean valid) 
+	{
+		this.valid = valid;
+	}
+
+	public ArrayList<PathData> getPathList() 
+	{
+		return pathList;
+	}
+
+	public void setPathList(ArrayList<PathData> pathList) 
+	{
+		this.pathList = pathList;
+	}
+
+	public NodeList getList() 
+	{
+		return list;
+	}
+
+	public void setList(NodeList list) 
+	{
+		this.list = list;
+	}
 
 }
